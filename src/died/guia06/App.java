f62492cde6c33c957d5072a034f9647c29b03e25
josesei <joseignaciocarbone@gmail.com>
2020-04-19 23:05:07 -0300

    resuelvo paso 09

--- src/died/guia06/App.java
+++ src/died/guia06/App.java
@@ -22,22 +22,22 @@ public class App {
 		Alumno a8 = new Alumno("Leandro", 8);
 		Alumno a9 = new Alumno("Leonardo", 9);
 		
-		c1.inscribir(a1);
-		c1.inscribir(a2);
-		c1.inscribir(a3);
-		c1.inscribir(a4);
-		c1.inscribir(a5);
-		c1.inscribir(a6);
-		c1.inscribir(a7);
-		c1.inscribir(a8);
-		c1.inscribir(a9);//falla por falta de cupo
-		c2.inscribir(a1);
-		c2.inscribir(a2);
-		c2.inscribir(a3);
-		c3.inscribir(a1);
-		c3.inscribir(a2);
-		c3.inscribir(a3);
-		c4.inscribir(a1); //falla por ser el 4to curso del mismo ciclo lectivo
+		c1.inscribirAlumno(a1);
+		c1.inscribirAlumno(a2);
+		c1.inscribirAlumno(a3);
+		c1.inscribirAlumno(a4);
+		c1.inscribirAlumno(a5);
+		c1.inscribirAlumno(a6);
+		c1.inscribirAlumno(a7);
+		c1.inscribirAlumno(a8);
+		c1.inscribirAlumno(a9);//falla por falta de cupo
+		c2.inscribirAlumno(a1);
+		c2.inscribirAlumno(a2);
+		c2.inscribirAlumno(a3);
+		c3.inscribirAlumno(a1);
+		c3.inscribirAlumno(a2);
+		c3.inscribirAlumno(a3);
+		c4.inscribirAlumno(a1); //falla por ser el 4to curso del mismo ciclo lectivo
 		
 		a1.aprobar(c1);
 		a1.aprobar(c2);
@@ -74,7 +74,7 @@ public class App {
 		c6.inscribir(a2);
 		
 		a3.aprobar(c4);
-		c5.inscribir(a3);
+		c5.inscribirAlumno(a3);
 		
 		System.out.println(a1.creditosObtenidos());
 		System.out.println(a2.creditosObtenidos());
@@ -100,9 +100,9 @@ public class App {
 		System.out.println(a8.creditosObtenidos());
 		System.out.println(a9.creditosObtenidos());
 		
-		c8.inscribir(a1);
-		c8.inscribir(a2);//créditos insuficientes
-		c8.inscribir(a3);//créditos insuficientes
+		c8.inscribirAlumno(a1);
+		c8.inscribirAlumno(a2);//créditos insuficientes
+		c8.inscribirAlumno(a3);//créditos insuficientes
 			
 		
 		
